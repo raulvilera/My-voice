b51ec79c26d04d32b7e6a3e01632bd3817efca6e
raulvilera <124225576+raulvilera@users.noreply.github.com>
2026-05-11 02:21:25 -0300

    Add routing with BrowserRouter to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, 
 Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Login from './pages/Login';
